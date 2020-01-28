008bbd9d9b6f29f830d7067623149a9e6723dfb8
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-01-28 16:18:26 +0200

    Test code of Add founction  #5

--- test.js
+++ test.js
@@ -2,6 +2,16 @@ var test = require('tape');
 var logic = require('./logic');
 
 test('Example test', function(t) {
-  t.pass();
-  t.end();
+    t.equal(1, 1, "the Test Run");
+    t.end();
+});
+
+
+
+//Test Of Add
+test("Test Add ", t => {
+    t.equal(logic.addTodo(), "the parameter can not be undefined");
+    t.equal(logic.addTodo([], ""), "the newToDo must be object link { id: 5, description: description, done: false }", "the newToDo must be object link { id: 5, description: description, done: false }")
+    t.equal(logic.addTodo("", { ere: 'dsf', }), "the toDO must be Array", "tthe toDO must be Array")
+    t.end();
 });
