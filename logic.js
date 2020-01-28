a638086409701e0d288751f5f6e84f5a416a867a
Hussein-shahatet97 <hussein.sh1997@hotmail.com>
2020-01-28 16:07:51 +0200

    add generateId to code  #1

--- logic.js
+++ logic.js
@@ -28,7 +28,7 @@ var todoFunctions = {
       // returns a new array, it should contain todos with the newTodo added to the end.
       // add an id to the newTodo. You can use the generateId function to create an id.
       // hint: array.concat
-
+      newTodo.id=this.generateId();
       var arr=this.cloneArrayOfObjects(todos);
       arr.push(newTodo);
       return arr;
