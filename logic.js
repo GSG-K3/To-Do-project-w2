2f9985970c13f7c27752427529cf5d4fcf44c446
Sahar <sa.froukh92@hotmail.com>
2020-01-29 11:30:04 +0200

    Add Delet code #3

--- logic.js
+++ logic.js
@@ -3,57 +3,89 @@
 // For part one we expect you to use tdd
 
 var todoFunctions = {
-    // todoFunctions.generateId() will give you a unique id
-    // You do not need to understand the implementation of this function.
-    generateId: (function() {
-      var idCounter = 0;
-  
-      function incrementCounter() {
-        return (idCounter += 1);
-      }
-  
-      return incrementCounter;
-    })(),
-    
-    //cloneArrayOfObjects will create a copy of the todos array 
-    //changes to the new array don't affect the original
-    cloneArrayOfObjects: function(todos) {
-      return todos.map(function(todo){
-        return JSON.parse(JSON.stringify(todo));
+  // todoFunctions.generateId() will give you a unique id
+  // You do not need to understand the implementation of this function.
+  generateId: (function () {
+    var idCounter = 0;
+
+    function incrementCounter() {
+      return (idCounter += 1);
+    }
+
+    return incrementCounter;
+  })(),
+
+  //cloneArrayOfObjects will create a copy of the todos array 
+  //changes to the new array don't affect the original
+  cloneArrayOfObjects: function (todos) {
+    return todos.map(function (todo) {
+      return JSON.parse(JSON.stringify(todo));
+    });
+  },
+
+  addTodo: function (todos, newTodo) {
+    // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
+    const newarr = this.cloneArrayOfObjects(todos);
+    console.log(newarr);
+
+    // returns a new array, it should contain todos with the newTodo added to the end.
+    // add an id to the newTodo. You can use the generateId function to create an id.
+    // hint: array.concat
+  },
+
+
+  deleteTodo: function (todos, idToDelete) {
+    // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
+    // return a new array, this should not contain any todo with an id of idToDelete
+    // hint: array.filter
+
+    if (todos == undefined || idToDelete == undefined) return "undefind data";
+    if (!(Array.isArray(todos))) return "It is not array";
+    if (!(Number.isInteger(idToDelete))) return "Invalid input ID";
+
+    if (todos.length == 0) return "It is Empty";
+
+
+    let array = this.cloneArrayOfObjects(todos);
+
+    let newArr = array.filter(
+      function (item) {
+        if (item.id != idToDelete) {
+          return true;
+        }
       });
-    },
-    
-    addTodo: function(todos, newTodo) {
-      // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
-      // returns a new array, it should contain todos with the newTodo added to the end.
-      // add an id to the newTodo. You can use the generateId function to create an id.
-      // hint: array.concat
-    },
-    deleteTodo: function(todos, idToDelete) {
-      // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
-      // return a new array, this should not contain any todo with an id of idToDelete
-      // hint: array.filter
-    },
-    markTodo: function(todos, idToMark) {
-      // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
-      // in the new todo array, all elements will remain unchanged except the one with id: idToMark
-      // this element will have its done value toggled
-      // hint: array.map
-    },
-    sortTodos: function(todos, sortFunction) {
-      // stretch goal! Do this last
-      // should leave the input arguement todos unchanged (you can use cloneArrayOfObjects)
-      // sortFunction will have same signature as the sort function in array.sort
-      // hint: array.slice, array.sort
-    },
-  };
-  
-  
-  // Why is this if statement necessary?
-  // The answer has something to do with needing to run code both in the browser and in Node.js
-  // See this article for more details: 
-  // http://www.matteoagosti.com/blog/2013/02/24/writing-javascript-modules-for-both-browser-and-node/
-  if (typeof module !== 'undefined') {
-    module.exports = todoFunctions;
-  }
-  
+    console.log(newArr);
+
+    // if two Array lenth is equal then the idToDelete not found in array
+    if (newArr.length == array.length) {
+      return "The task does not exist";
+    }
+
+    // return new Array after delete item
+    return newArr;
+
+  },
+  markTodo: function (todos, idToMark) {
+    // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
+    // in the new todo array, all elements will remain unchanged except the one with id: idToMark
+    // this element will have its done value toggled
+    // hint: array.map
+
+
+  },
+  sortTodos: function (todos, sortFunction) {
+    // stretch goal! Do this last
+    // should leave the input arguement todos unchanged (you can use cloneArrayOfObjects)
+    // sortFunction will have same signature as the sort function in array.sort
+    // hint: array.slice, array.sort
+  },
+};
+
+
+// Why is this if statement necessary?
+// The answer has something to do with needing to run code both in the browser and in Node.js
+// See this article for more details: 
+// http://www.matteoagosti.com/blog/2013/02/24/writing-javascript-modules-for-both-browser-and-node/
+if (typeof module !== 'undefined') {
+  module.exports = todoFunctions;
+}
--- test.js
+++ test.js
@@ -1,7 +1,16 @@
 var test = require('tape');
 var logic = require('./logic');
 
-test('Example test', function(t) {
-  t.pass();
+
+test('Delete Test ', function (t) {
+  let arr = [
+    { id: 0, description: 'make tea', done: false },
+    { id: 1, description: 'make eggs', done: true }];
+  let expected = [{ id: 0, description: 'make tea', done: false }];
+  t.equal(logic.deleteTodo(), "undefind data", 'This is not array')
+  t.equal(logic.deleteTodo("", 1), "It is not array", 'This is not array')
+  t.equal(logic.deleteTodo([], ""), "Invalid input ID", 'It is not an integer')
+  t.equal(logic.deleteTodo([], 1), "It is Empty", ' you are trying to deletr empty array')
+  t.deepEqual(logic.deleteTodo(arr, 2), "The task does not exist", "you are trying to delet not found task");
   t.end();
 });
