39c063a2b4a071ddc262f89d89237234939b4ac6
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-01-29 10:05:14 +0200

    Test Add #6

--- test.js
+++ test.js
@@ -8,10 +8,21 @@ test('Example test', function(t) {
 
 
 
+// Test generate id
+test("Test generate id", x => {
+    x.equal(logic.generateId(), 1, "The id is not unique");
+    x.equal(logic.generateId(), 2, "The id is not unique");
+    x.equal(logic.generateId(), 3, "The id is not unique");
+    x.equal(logic.generateId(), 4, "The id is not unique");
+    x.end();
+});
+
 //Test Of Add
 test("Test Add ", t => {
     t.equal(logic.addTodo(), "the parameter can not be undefined");
     t.equal(logic.addTodo([], ""), "the newToDo must be object link { id: 5, description: description, done: false }", "the newToDo must be object link { id: 5, description: description, done: false }")
     t.equal(logic.addTodo("", { ere: 'dsf', }), "the toDO must be Array", "tthe toDO must be Array")
+
+
     t.end();
 });
